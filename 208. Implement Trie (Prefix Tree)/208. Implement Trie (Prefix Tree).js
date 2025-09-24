c35420ab5e2f5fe93b3d92e5e306f1c6cb846347
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-09-25 00:34:23 +1200

    solutions added

---
+++ 208. Implement Trie (Prefix Tree)/208. Implement Trie (Prefix Tree).js
@@ -0,0 +1,69 @@
+class TrieNode {
+  constructor() {
+    this.children = new Map();
+    this.endOfWord = false;
+  }
+}
+
+var Trie = function () {
+  this.root = new TrieNode();
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+  let curr = this.root;
+
+  for (const c of word) {
+    if (!curr.children.has(c)) {
+      curr.children.set(c, new TrieNode());
+    }
+    curr = curr.children.get(c);
+  }
+
+  curr.endOfWord = true;
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (word) {
+  let curr = this.root;
+
+  for (const c of word) {
+    if (!curr.children.has(c)) {
+      return false;
+    }
+    curr = curr.children.get(c);
+  }
+
+  return curr.endOfWord;
+};
+
+/**
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+  let curr = this.root;
+
+  for (const c of prefix) {
+    if (!curr.children.has(c)) {
+      return false;
+    }
+    curr = curr.children.get(c);
+  }
+
+  return true;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
