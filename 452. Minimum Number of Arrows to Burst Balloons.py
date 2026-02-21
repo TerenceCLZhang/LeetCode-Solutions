a5db3db2c7d98d988213a686a08686207675f35a
TerenceCLZhang <terence.zhang2014@gmail.com>
2026-02-21 16:30:44 +1100

    solutions added

---
+++ 452. Minimum Number of Arrows to Burst Balloons.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        points.sort()
+        
+        ans = len(points)
+
+        prev = points[0]
+        for i in range(1, len(points)):
+            curr = points[i]
+
+            if curr[0] <= prev[1]:
+                ans -= 1
+                prev = [curr[0], min(prev[1], curr[1])]
+            else:
+                prev = curr
+
+        return ans
+        
--- README.md
+++ README.md
@@ -5,6 +5,6 @@ A repository of Python LeetCode solutions for commonly asked interview problems.
 - [ ] NeetCode 150
 - [ ] Blind 75
 - [x] Grind 75
-- [ ] LeetCode 75
+- [x] LeetCode 75
 - [x] Algomap.io ([Greg Hogg](https://www.youtube.com/@GregHogg))
 - [ ] [stoney codes](https://www.youtube.com/watch?v=lvO88XxNAzs)
